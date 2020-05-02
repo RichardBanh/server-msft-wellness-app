7d6cfff7e46be286ba39b740b96559a8df065823
rbanh <rbanh@me.com>
2020-05-02 15:56:19 -0700

    added configs and error codes

--- Middleware/middleware.js
+++ Middleware/middleware.js
@@ -1,9 +1,15 @@
+const consoleLogger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  var start = process.hrtime();
 
-const consoleLogger = (req, res, next)=> {
-  console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)
-  next()
-}
-
-
+  res.on("finish", function () {
+    var hrtime = process.hrtime(start);
+    var elapsed = parseFloat(hrtime[0] + (hrtime[1] / 1000000).toFixed(3), 10);
+    console.log(elapsed + "ms");
+  });
+  next();
+};
 
 module.exports = consoleLogger;
--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+dotenv.config({ path: "./Config/config.env" });
 const app = express();
 
-const consoleLogger = require("./Middleware/middleware")
+const connectData = require('./Config/db')
+
+connectData()
+
+const consoleLogger = require("./Middleware/middleware");
 
 //CRUD
-
-const dataRoute = require('./Routes/dataRoute')
+const dataRoute = require("./Routes/dataRoute");
 
 // middleware
-app.use(consoleLogger)
+app.use(consoleLogger);
 
 //routes
-app.use('/', dataRoute);
+app.use("/", dataRoute);
 
+//configuration
+const port = process.env.PORT || 5000; // change it in the terminal 'export PORT=5000'
+const serve = app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// app.post();
-// app.put();
-// app.delete();
-
-//configuration 
-const port = process.env.PORT || 3000; // change it in the terminal 'export PORT=5000'
-app.listen(port, () => console.log(`Listening on port ${port}`));
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error is: ${err.message}`);
+  serve.close(() => process.exit(1))
+})
